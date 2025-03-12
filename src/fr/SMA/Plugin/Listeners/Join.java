341d8afc224eee8b33ff919bd955e7cd17817787
valthys10 <mathys1098@hotmail.com>
2025-03-12 20:55:09 +0100

    Joining Listeners

---
+++ src/fr/SMA/Plugin/Listeners/Join.java
@@ -0,0 +1,14 @@
+package fr.SMA.Plugin.Listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Join implements Listener {
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		
+	}
+
+}
