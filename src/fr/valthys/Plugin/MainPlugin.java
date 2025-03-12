389ec6af82a45e5803b590e4d40cc6e0c007af96
valthys10 <mathys1098@hotmail.com>
2025-03-12 19:03:43 +0100

    Add the file of the plugin

---
+++ src/fr/valthys/Plugin/MainPlugin.java
@@ -0,0 +1,16 @@
+package fr.valthys.Plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+	
+}
